641faa91fafc99e78ba388f77239fb0fbdff6146
supereko <lenkozina@gmail.com>
2019-08-06 22:49:18 +1000

    сообщение

---
+++ tele.py
@@ -0,0 +1,18 @@
+import telebot
+
+
+bot = telebot.TeleBot('926166838:AAHFskxOyJeRDyJUT9m1tDeyFQBryn24ItY')
+
+@bot.messages_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text == 'Привет':
+        bot.send_message(message.chat.id, 'Привет, мой создатель')
+    elif message.text == 'Пока':
+        bot.send_message(message.chat.id, 'Прощай, создатель')
+
+
+bot.polling()
